c3775acc4ce1571ab2b4d12b61c6a20a108a8b7d
riadh azouzi <azouzi.riadhanis@gmail.com>
2022-03-05 14:16:44 +0700

    added similarity.py to compute player similarities and modified a few things

---
+++ opti_recruit/similarity.py
@@ -0,0 +1,70 @@
+import opti_recruit.feature_engineering as fe
+import opti_recruit.get_team_features as gtf
+import pandas as pd
+import numpy as np
+from sklearn.pipeline import Pipeline,make_pipeline,make_union
+from sklearn.compose import make_column_transformer,make_column_selector
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler,OneHotEncoder
+from opti_recruit.data import get_data, clean_data
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.neighbors import NearestNeighbors
+
+
+
+def get_index(df,player):
+    return df[df['short_name']==player].index.tolist()[0]
+
+def normalize(array):
+    return np.array([round(num, 2) for num in (array - min(array))*100/(max(array)-min(array))])
+
+def numeric_pipeline(df):
+    num_transformer = make_pipeline(SimpleImputer(), StandardScaler())
+    num_col = make_column_selector(dtype_include=['float64','int64'])
+
+    cat_transformer = OneHotEncoder()
+    cat_col = make_column_selector(dtype_include=['object','category'])
+
+    preproc_basic = make_column_transformer(
+        (num_transformer, num_col),
+        (cat_transformer, cat_col),
+        remainder='passthrough')
+
+    SimpleImputer.get_feature_names_out = (lambda self, names=None: self.feature_names_in_)
+
+    num_trans_df = preproc_basic.fit_transform(df)
+
+    sim_df = pd.DataFrame(num_trans_df,
+                columns=preproc_basic.get_feature_names_out()
+            )
+    return sim_df
+
+def get_similarity_dataframe(df):
+    """Extract and transform the original dataframe to have only numerical feature"""
+    to_drop = ['sofifa_id','short_name','player_positions','height_cm','weight_kg','club_team_id'
+               ,'club_name' ,'league_name','club_position','club_joined',
+               'club_contract_valid_until','nationality_name','nation_team_id',
+               'preferred_foot','weak_foot','work_rate','body_type',
+               'player_tags','player_traits','is_bench','potential_diff',
+               'age_bin','player_pos','new_nationality','value_eur','wage_eur','release_clause_eur']
+
+    num_df = df.drop(to_drop, axis = 1)
+
+    similarity_df = numeric_pipeline(num_df)
+
+    # Cosine Similarity matrix
+    similarities = cosine_similarity(similarity_df)
+    # KNN matrix
+    # reco = NearestNeighbors(n_neighbors=11, algorithm='ball_tree').fit(similiraty_df)
+
+    return similarities
+
+def cosine_recommendation(df,player):
+    index = get_index(df,player)
+    similarities = get_similarity_dataframe(df)
+    norm_sim_array= np.sort(normalize(similarities[index,:]))[-11:][::-1][1:]
+    index_matrix = similarities[index,:].argsort()[-11:][::-1][1:]
+    dict_simili = {'index_player': index_matrix,'score':norm_sim_array}
+    reco_df = pd.DataFrame(dict_simili).set_index('index_player')
+    reco_df['sofifa_id'] = df.iloc[reco_df.index]['sofifa_id']
+    return reco_df
